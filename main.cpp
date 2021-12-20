16e8d8c05f8ce1e9b5ca0ad96fadb50fc0322527
Vu Dang Khoa <87051112+khoavukhoa3004@users.noreply.github.com>
2021-12-20 10:46:38 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "SymbolTable.h"
+#include "SymbolTable.cpp"
+using namespace std;
+
+void test(string filename)
+{
+    SymbolTable *st = new SymbolTable();
+    try
+    {
+        st->run(filename);
+    }
+    catch (exception &e)
+    {
+        cout << e.what();
+    }
+    delete st;
+}
+
+int main()
+{
+    test("test1.txt");
+    return 0;
+}
